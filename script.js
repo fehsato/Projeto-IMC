c5672e96ef980c0cadc710bd81543c7d081ab80e
Fernanda Yonezawa Sato <100849359+fehsato@users.noreply.github.com>
2023-03-07 10:04:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function somar{
+    var peso, altura, result};
+    peso = parseFloat(document.getElementById("peso").value);
+    altura = parseFloat(document.getElementById("altura").value);
+
+    result = pesp + altura;
+
+    
