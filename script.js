35bda107c9ce51b5a50788dbbe1ae3298a04d9d5
Fernanda Yonezawa Sato <100849359+fehsato@users.noreply.github.com>
2023-03-20 08:54:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,36 @@ function calcularIMC() {
     altura = parseFloat(document.getElementById("altura").value);
     altura = altura / 100;
     result = peso / (altura*altura);
+    
+    const textResult = document.getElementById("txt-resultado");
 
-    document.getElementById('result').value = result;
+    
+    
+
+    // alert(Math.round(result));
+
+    if(result < 18.5){
+        textResult.innerHTML = "Magreza"
+    }
+    
+    if( 24.9 < result || result > 18.5){
+        textResult.innerHTML = "Normal"
+    }
+
+    if( 29.9 < result || result > 25.0){
+    textResult.innerHTML = "Sobrepeso"
+    }
+
+    if( 34.9 < result || result > 30){
+        textResult.innerHTML = "Obesidade grau I"
+    }
+
+    if( 39.9 < result || result > 35){
+        textResult.innerHTML = "Obesidade grau II"
+    }
+    if( result > 40){
+        textResult.innerHTML = "Obesidade grau III"
+    }
 }
+
+
